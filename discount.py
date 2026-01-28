f1486e473be6fc9ee4d8d01f757e015a971f423b
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-01-28 21:35:06 +0530

    python practice on discount

---
+++ discount.py
@@ -0,0 +1,5 @@
+#a shop will givr discount of 10% if the cost of purchased quality is more than 1000
+bill=float(input("ENTER THR BILL Amount: "))
+if bill>1000:
+    bill =bill-bill*0.1
+print("The paybill Amount is: ",bill)
