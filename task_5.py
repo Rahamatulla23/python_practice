be1eda897bbf0ab36424cc86ddc8b162cc43d438
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-01-29 22:16:27 +0530

    pattern program practice

---
+++ task_5.py
@@ -0,0 +1,13 @@
+size=6
+m=(2*size)-2
+for i in range(0,size):
+    for j in range(0,m):
+        print(end="")
+    m=m-1
+    for j in range(0,i+1):
+         print("^_^",end="")
+    print()
+    '''
+    for j in range(0,10):
+    print(end="")
+    '''
