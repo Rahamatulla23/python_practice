3f8b47b501ad6b33f273cedbbbcc4099fed53500
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-02 22:26:16 +0530

    while loop practice

---
+++ while_1.py
@@ -0,0 +1,11 @@
+#write a python pogram to display between 1 to 100 using while loop
+'''i=1
+while i<=100:
+    print(i)
+    i+=1
+''' 
+#or  
+i=0
+while i in range(101):   # check if i is within the range
+    print(i)
+    i += 1
