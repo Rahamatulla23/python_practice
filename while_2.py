52a9d777d1106c8a3231db47b09450a67551b5db
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-04 22:33:32 +0530

    while loop practice 2

---
+++ while_2.py
@@ -0,0 +1,11 @@
+#write a python program to display the even numbers by using while loop
+'''i =1
+while i<=100:
+    if i%2!=1:
+        print("the even numbers ",i)
+    i+=1'''
+#or
+i =2 
+while i<=100:
+    print(i)
+    i+=2
