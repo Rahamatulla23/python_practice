4308f9d58f0130a8217d0a70a8cd06193926221c
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-07 23:15:21 +0530

    for loop practice 9

---
+++ for_loop_ep9.py
@@ -0,0 +1,7 @@
+#Write code to create a list of word lengths for the words in original_str using the accumulation pattern and assign the answer to a variable num_words_list. (You should use the len function).
+original_str = "The quick brown fox jumpes over the extremely lazy dog."
+num_words_list = []
+for ele in original_str.split():
+    num_words_list.append(len(ele))
+
+print(num_words_list)
