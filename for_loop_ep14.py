b3e50b337198e7f591003914c2181f7de8fd3632
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-08 22:09:36 +0530

    for loop practice 14

---
+++ for_loop_ep14.py
@@ -0,0 +1,7 @@
+#addition_str is a string with a list of numbers separated by the + sign. Write code that uses the accumulation pattern to take the sum of all of the numbers and assigns it to sum_val (an integer). (You should use the .split("+") function to split by "+" and int() to cast to an integer).
+addition_str = "2+5+10+20"
+sum_val = 0
+for ele in addition_str.split('+'):#'2', '5', '10', '20'
+    sum_val = sum_val + int(ele)
+
+print(sum_val)
