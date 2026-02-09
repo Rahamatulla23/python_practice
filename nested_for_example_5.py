81eb8a78f43acf3b7e29f2cff0d443f460a6de7a
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-09 21:30:24 +0530

    nested for loop practice 5

---
+++ nested_for_example_5.py
@@ -0,0 +1,11 @@
+start = 1
+stop = 2
+num = stop
+for row in range(2, 6):
+   for col in range(start, stop):
+     num = num - 1
+     print (num, end=' ')
+   print("")
+   start = stop
+   stop +=row
+   num = stop
