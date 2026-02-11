fd893406336fd92037ff971b558861f27b3e8a26
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-11 23:20:01 +0530

    String Practice 1

---
+++ string_example_1.py
@@ -0,0 +1,4 @@
+#1) Write a Python program that takes a list of words and returns the length of the longest one.
+
+s=['Ram','Rahim','Robert','GuruGovind','Mahaveer','Anantapur']
+print(max(s, key = len))
