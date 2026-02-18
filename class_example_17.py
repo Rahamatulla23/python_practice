003145ba2e10d44df2be83e30f4c83f36973518d
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-18 23:10:02 +0530

    class ep17

---
+++ class_example_17.py
@@ -0,0 +1,14 @@
+class Myclass: 
+   n=0 
+   def __init__(self): 
+      Myclass.n = Myclass.n+1 
+
+   @staticmethod 
+   def noObjects(): 
+      print('No. of instances created: ', Myclass.n) 
+
+obj1 = Myclass() 
+obj2 = Myclass() 
+obj3 = Myclass() 
+Myclass.noObjects()
+
