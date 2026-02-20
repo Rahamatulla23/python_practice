05f4a1bb7b0b7f0ad0bc2b0bb8b100ea227f1dd8
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-20 22:31:35 +0530

    super method 2

---
+++ super_method_2.py
@@ -0,0 +1,17 @@
+class Square: 
+   def __init__(self, x): 
+      self.x = x 
+   def area(self): 
+      print('Area of square=', self.x*self.x) 
+
+class Rectangle(Square): 
+   def __init__(self, x, y): 
+      super().__init__(x) 
+      self.y = y
+   def area(self): 
+      super().area() 
+      print('Area of rectangle=', self.x*self.y) 
+      
+a,b = [float(x) for x in input("Enter two measurements: ").split()] 
+r = Rectangle(a,b) 
+r.area()
