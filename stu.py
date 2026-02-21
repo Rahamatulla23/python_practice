19c4e7771eceb536028db6d89071c248ef26c293
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-21 22:51:57 +0530

    oops practice

---
+++ stu.py
@@ -0,0 +1,18 @@
+from teach import *
+class Student(Teacher): 
+
+   def setmarks(self, marks): 
+      self.marks = marks 
+   def getmarks(self): 
+      return self.marks
+
+s = Student() 
+s.setid(100) 
+s.setname('Rakesh')
+s.setaddress('H.No.-231, Old Town, Anantapur') 
+s.setmarks(970)
+
+print('id=', s.getid()) 
+print('name=', s.getname()) 
+print('address=', s.getaddress()) 
+print('marks=', s.getmarks())
