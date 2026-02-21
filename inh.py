c28b686bec55bfde91934dabd4a0babb99acd41b
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-21 22:51:37 +0530

    oops practice

---
+++ inh.py
@@ -0,0 +1,12 @@
+from teach import *
+t = Teacher() 
+
+t.setid(10) 
+t.setname('Prakash') 
+t.setaddress('H.No.-29, Neeruganti Street, Anantapur') 
+t.setsalary(25000.50) 
+
+print('id=', t.getid()) 
+print('name=', t.getname()) 
+print('address=', t.getaddress()) 
+print('salary=', t.getsalary())
