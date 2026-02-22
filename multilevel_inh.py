2c016d725eb43007892d03510e115c7cf47f3de6
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-22 22:27:13 +0530

    multilevel inheritance

---
+++ multilevel_inh.py
@@ -0,0 +1,15 @@
+class A(object):
+    def methodA(self):
+        print("This Method is in class A")
+
+class B(A):
+    def methodB(self):
+        print("This Method is in class B")
+class C(B):
+    def methodC(self):
+        print("This Method is in class C")
+
+obj = C()
+obj.methodA()
+obj.methodB()
+obj.methodC()
