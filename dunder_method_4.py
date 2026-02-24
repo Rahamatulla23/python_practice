81d335a36b195144c39fa211c2b09dc3e2e17cbb
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-24 22:30:01 +0530

    oops practice

---
+++ dunder_method_4.py
@@ -0,0 +1,15 @@
+class Employee: 
+   def __init__(self, name, salary): 
+      self.name = name 
+      self.salary = salary 
+   def __mul__(self, other): 
+      return self.salary*other.days 
+
+class Attendance: 
+   def __init__(self, name, days): 
+      self.name = name 
+      self.days = days 
+
+x1 = Employee('Srinu', 500.00) 
+x2 = Attendance('Srinu', 25) 
+print('This month salary=', x1*x2)
