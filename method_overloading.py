f9b623bf753b917275607ac11dce498a37ef7a41
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-02-24 22:26:39 +0530

    method overloading

---
+++ method_overloading.py
@@ -0,0 +1,11 @@
+class Sample:
+    def add(self, a,b):
+        ans = a + b
+        print("Sum =",ans)
+    
+    def add(self, a,b,c):
+        ans = a+ b + c
+        print("Sum =",ans)
+
+obj = Sample()
+obj.add(34,5,8)
