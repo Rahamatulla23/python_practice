47e7b0ffe35750df2caf0c1b0d2359d6df1860ea
Shaik Mahammad Rahamatulla <rahamatullashaik1382@gmail.com>
2026-03-01 22:25:31 +0530

    exception handling 7

---
+++ exception_handling_7.py
@@ -0,0 +1,15 @@
+def avg(list): 
+   tot=0 
+   for x in list: #1,2,3,4,5,'a'
+      tot+=x 
+   avg = tot/len(list) 
+   return tot, avg 
+
+try: 
+   t,a = avg([1,2,3,4,5,'a']) 
+
+   print('Total= {}, Average= {}'.format(t,a)) 
+except TypeError: 
+   print('Type Error, please provide numbers.') 
+except ZeroDivisionError: 
+   print('ZeroDivisionError, please do not give empty list.')
